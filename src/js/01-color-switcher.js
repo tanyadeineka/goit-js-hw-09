3f1872a13d3e9b347bd4478fb0dc36c0c0071fd1
Tetiana Deineka <tata4ka666@i.ua>
2022-11-14 17:45:26 +0200

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,23 @@
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector('button[data-stop]');
+const body = document.querySelector('body');
 
+let timerId = null;
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+startBtn.addEventListener('click', () => {
+  timerId = setInterval(() => {
+      body.style.backgroundColor = getRandomHexColor();
+      startBtn.disabled = true;
+      stopBtn.disabled = false;
+  }, 1000);
+});
+
+stopBtn.addEventListener('click', () => {
+  clearInterval(timerId);
+    stopBtn.disabled = true;
+    startBtn.disabled = false;
+});
